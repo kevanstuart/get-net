e0248c1922f493040fe8d7a198da007547d1d74f
Kevan Stuart <kevan.jedi@gmail.com>
2017-10-03 06:56:56 +0700

    Testing gitignore deleted files

--- routes/data_routes.js
+++ routes/data_routes.js
@@ -5,9 +5,28 @@
  * Handles routing for the data api urls
  */
 
+
+/**
+ * Require database connector
+ */
 const db = require('../connectors/psql');
 
 
+/**
+ * Filters to check POST data against
+ */
+var filtersToCheck = ['price', 'download', 'provider', 'connection_type'];
+
+
+/**
+ * Sort Criteria to check POSt data against
+ */
+var sortByToCheck  = ['download', 'upload', 'price', 'price & upload'];
+
+
+/**
+ * Data API Routes function
+ */
 module.exports = function(application, config)
 {
 	'use strict';
@@ -15,21 +34,224 @@ module.exports = function(application, config)
 	/**
 	 * Set config into request
 	 */
-	application.use(function(req, res, next) 
+	/*application.use(function(req, res, next) 
 	{
 		req.config = config;
 		next();
-	});
-
+	});*/
 
 	/**
 	 * Set data endpoint to send JSON back to the frontend
 	 */
 	application.get('/data', function(req, res)
 	{
-		let result = db.getFilteredPlans();
-		/*result.then(function (then) {
+		let result = db.getAllPlans();
+		result.then(function (then) {
 			res.json(then.rows);
-		});*/
+		});
     });
+
+	/**
+	 * Set data endpoint to take POST data and return JSON to the frontend
+	 */
+    application.post('/data', function(req, res) 
+    {
+
+    	/**
+    	 * Get post data from body
+    	 */
+    	let postData = req.body;
+
+    	/**
+    	 * Get data and handle promise
+    	 */
+    	let result = db.getAllPlans();
+    	result.then(function (data) 
+    	{
+
+    		/**
+    		 * Init rows array
+    		 */
+    		let rows = data.rows;
+
+    		/**
+    		 * Filter rows
+    		 */
+    		let filtered = filterResults(rows, postData);
+
+    		/**
+    		 * Sort filtered rows
+    		 */
+    		let sorted   = sortResults(filtered, postData);
+
+    		/**
+    		 * Send filtered results to Frontend
+    		 */
+    		res.json(sorted);
+
+    	});
+    	
+    });
+}
+
+
+/**
+ * Filter the results from the database with the provided POST data
+ */
+function filterResults(rows, postData)
+{
+
+	/**
+	 * Set the filters to loop through
+	 */
+	let filters = getRequestedFilters(Object.keys(postData));
+
+	/**
+	 * Filter rows && return new Array
+	 */
+	return rows.filter(function(row) 
+	{
+
+		/**
+		 * Push filter results to new Array
+		 */
+		let pass = new Array();
+		filters.forEach(function (item) {
+			pass.push(getFilterComparisons(item, row, postData));
+		});
+
+		/**
+		 * Return false if a filter fails
+		 */
+		if (pass.includes(false))
+		{
+			return false;
+		}
+		return true;
+
+	});
+
+}
+
+
+/**
+ * Explode filters into column && operator in an Object
+ */
+function getRequestedFilters(filters)
+{
+
+	/**
+	 * Loop through filters
+	 */
+	filters.forEach(function(item, index) 
+	{
+
+		/**
+		 * Create object and split item into array
+		 */
+		let tmpObj = new Object();
+		let tmpArr = item.split('_');
+
+		/**
+		 * Populate Object
+		 */
+		tmpObj[ tmpArr[1] ] = tmpArr[0];
+		filters[index] = tmpObj;
+
+	});
+
+	/**
+	 * Return exploded filters
+	 */
+	return filters;
+
+}
+
+
+/**
+ * Return boolean on comparing values to filters
+ */
+function getFilterComparisons(filter, element, postData)
+{
+
+	/**
+	 * Get column and operator
+	 */
+	let column   = Object.keys(filter)[0];
+	let operator = filter[column];
+
+	/**
+	 * Perform comparison operation and return
+	 */
+	if (operator === 'min')
+	{
+		return element[column] >= postData[ operator + '_' + column ];
+	}
+	else if (operator === 'max')
+	{
+		return element[column] <= postData[ operator + '_' + column ];
+	}
+
+}
+
+
+/**
+ * Sort the filtered rows with the provided POST data
+ */
+function sortResults(rows, postData)
+{
+
+	/**
+	 * Explode the Sorting POST data into sort && direction
+	 */
+	var sortObj = getSortAndDirection(postData.sort);
+	var sortKey = Object.keys(sortObj)[0];
+	var sortDir = sortObj[sortKey];
+
+	/**
+	 * Sort the rows
+	 */
+	rows.sort(function(a, b) 
+	{
+
+		/**
+		 * Change sort calculation for directionality
+		 */
+		if (sortDir == "desc")
+		{
+			return b[sortKey] - a[sortKey];
+		}
+		else if (sortDir == "asc")
+		{
+			return a[sortKey] - b[sortKey];
+		}
+		
+	});
+
+	/**
+	 * Return sorted rows
+	 */
+	return rows;
+
+}
+
+
+/**
+ * Explode the Sorting POST data into sort && direction
+ */
+function getSortAndDirection(item)
+{
+
+	/**
+	 * Create object and split item into array
+	 */
+	let tmpObj = new Object();
+	let tmpArr = item.split('_');
+
+	/**
+	 * Populate Object && return
+	 */
+	tmpObj[ tmpArr[0] ] = tmpArr[1];
+	return tmpObj;
+
 }

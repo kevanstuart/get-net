79cc86b91711124172f0d31038ebb81694447ab6
Kevan Stuart <kevan.jedi@gmail.com>
2017-10-07 17:06:28 +0700

    Removed console log

--- routes/api.js
+++ routes/api.js
@@ -44,9 +44,7 @@ module.exports = function(application, config, db)
 	 */
 	application.get('/data', function(req, res)
 	{
-
-		console.log(db);
-
+		
 		/**
 		 * Get all results && push to json
 		 */

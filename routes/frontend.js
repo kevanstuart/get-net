87a55036ebfecbefb9e04948f049a0791a55c7f8
Kevan Stuart <kevan.jedi@gmail.com>
2017-11-07 19:15:39 +0700

    Bug in getting filters

--- routes/frontend.js
+++ routes/frontend.js
@@ -159,8 +159,8 @@ module.exports = function(application, config)
 		{
 
 			console.log(error);
-			console.log(response);
-			
+			//console.log(response);
+
 			/*// Assign data and add filters
 			let appData = JSON.parse(data);
 			appData.filters = res.locals.filters;
